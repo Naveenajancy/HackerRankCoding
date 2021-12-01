cae27c33fde7429702b9b96a63260218844eae18
nkb05 <kb.jancy90@gmail.com>
2021-12-01 11:52:58 -0500

    adding java test solution.

---
+++ src/CodingPractice/strings/JavaTest.java
@@ -0,0 +1,28 @@
+package CodingPractice.strings;
+
+public class JavaTest {
+    public static void main(String[] args) {
+        int i = 010;
+        int j = 07;
+        System.out.println(i + ","+ j);
+
+        try{
+            Float f = new Float("3.0");
+            int x = f.intValue();
+            byte b = f.byteValue();
+            double d = f.doubleValue();
+            System.out.println(x +" , " + b + " ," +d);
+            System.out.println(x+b+d);
+            }catch (NumberFormatException e){
+            System.out.println("bad nu");
+
+        }
+
+    }
+}
+
+
+/*gnakshgashgrhgi *****/
+///*sdgasg////sgagsagh*/
+///*more comment /* more asfsaf */*/
+
